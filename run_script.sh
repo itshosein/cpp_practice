9ed15fe01b9d1011d39cbd2949880e468bcd2488
Hosein Fathi <hfathi711@gmail.com>
2024-10-20 12:34:01 +0330

    fixed config

--- run_script.sh
+++ run_script.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 clear
-# if [ -f ./build/src/output.exe ]; then {
-#     echo "Deleting previous output file..."
-#     rm ./build/mingw-debug/src/output.exe
-#     if [ $? -eq 0 ]; then {
-#         echo "The file deleted successfully."
-#     } else {
-#         echo "Could not delete the file."
-#     } fi
-# } fi
 
-cmake -S . -B build/mingw-debug --preset=default
-echo "******* (cmake -S . -B build/mingw-debug --preset default) done *******"
+build_location=build/mingw-debug
+if [ -f ./$build_location/src/output.exe ]; then {
+    echo "Deleting previous output file..."
+    rm ./build/mingw-debug/src/output.exe
+    if [ $? -eq 0 ]; then {
+        echo "The file deleted successfully."
+    } else {
+        echo "Could not delete the file."
+    } fi
+} fi
 
-cmake --build build/mingw-debug
-echo "******* (cmake --build build/mingw-debug) done *******"
+cmake -S . -B $build_location --preset=default
+echo "******* (cmake -S . -B $build_location --preset default) done *******"
+
+cmake --build $build_location
+echo "******* (cmake --build $build_location) done *******"
 
 if [ $? -eq 0 ]; then {
     # clear
     echo "app output{"
-    ./build/mingw-debug/src/output.exe  # Adjust if needed
+    ./$build_location/src/output.exe  # Adjust if needed
     echo "}"
 } else {
     echo "Build failed."
