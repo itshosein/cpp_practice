fffe1208d217922c6cdb79236fb4e3e158218b93
Hosein Fathi <hfathi711@gmail.com>
2024-11-11 12:08:02 +0330

    added example function callback as argument

--- src/practice_files/function_like_entities/function_entities.cpp
+++ src/practice_files/function_like_entities/function_entities.cpp
@@ -5,7 +5,7 @@
 namespace FunctionEntities {
 void function_entities() {
   /* add or &add or with = */
-  double (*add_ptr1)(double, double){&add};
+  /* double (*add_ptr1)(double, double){&add};
   double (*add_ptr2)(double, double){nullptr};
   add_ptr2 = &add;
 
@@ -16,8 +16,52 @@ void function_entities() {
   fmt::print(fg(fmt::color::blue), "add_ptr2(10,1212.13): {}\n",
              add_ptr2(10, 1212.13));
   fmt::print(fg(fmt::color::blue), "add_ptr3(10,1212.13): {}\n",
-             add_ptr3(10, 1212.13));
+             add_ptr3(10, 1212.13)); */
+
+  std::string msg{"I am c++ developer!"};
+  std::string msg1{"I am a better c++ developer!"};
+
+  modify(msg, encrypt);
+  fmt::print(fg(fmt::color::blue), "msg encrypted: {}\n", msg);
+  modify(msg, decrypt);
+  fmt::print(fg(fmt::color::blue), "msg decrypted: {}\n", msg);
+
+  std::vector<std::string> str_v{};
+  str_v.push_back(msg);
+  str_v.push_back(msg1);
+  str_v.emplace_back("WOw that is awesome");
+  fmt::print(fg(fmt::color::blue), "msg is best or msg2: {}\n",
+             get_best(str_v, large_in_size));
 }
 
 double add(double a, double b) { return a + b; }
+
+char encrypt(const char &input) { return static_cast<char>(input + 3); }
+char decrypt(const char &input) { return static_cast<char>(input - 3); }
+
+std::string &modify(std::string &input, char (*fn)(const char &)) {
+  for (size_t i{}; i < input.size(); i++) {
+    input[i] = fn(input[i]);
+  }
+  return input;
+}
+
+bool large_in_size(const std::string &a, const std::string &b) {
+  return a.size() > b.size();
+}
+
+std::string get_best(std::vector<std::string> str_v,
+                     bool (*comparator)(const std::string &a,
+                                        const std::string &b)) {
+  std::string best{};
+  for (size_t i{}; i < str_v.size(); i++) {
+    for (auto &j : str_v) {
+      if (comparator(str_v[i], j)) {
+        best = str_v[i];
+      }
+    }
+  }
+  return best;
+}
+
 } // namespace FunctionEntities
--- src/practice_files/function_like_entities/include/function_entities.hpp
+++ src/practice_files/function_like_entities/include/function_entities.hpp
@@ -1,5 +1,18 @@
+#include <string>
+#include <vector>
 namespace FunctionEntities {
 void function_entities();
 
 double add(double a, double b);
+
+char encrypt(const char &);
+char decrypt(const char &);
+
+std::string &modify(std::string &input, char (*fn)(const char &));
+
+std::string get_best(std::vector<std::string> str_v,
+                     bool (*comparator)(const std::string &a,
+                                        const std::string &b));
+
+bool large_in_size(const std::string &a, const std::string &b);
 } // namespace FunctionEntities
