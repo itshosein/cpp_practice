a824989255d7d2bc013b99ad2a02f179febe8c0d
Hosein Fathi <hfathi711@gmail.com>
2024-11-11 15:40:21 +0330

    added vector of stl

--- src/practice_files/stl/include/stl_func.hpp
+++ src/practice_files/stl/include/stl_func.hpp
@@ -1,3 +1,9 @@
+#include <vector>
+#include <cstring>
+
 namespace StlExercise {
 void stl_func();
+
+template <typename T>
+void print_raw_array(const T *p, size_t size);
 } // namespace StlExercise
--- src/practice_files/stl/stl_func.cpp
+++ src/practice_files/stl/stl_func.cpp
@@ -1,3 +1,7 @@
+#include "stl_func.hpp"
+#include <fmt/color.h>
+#include <fmt/core.h>
+
 namespace StlExercise {
 void stl_func() {
   /*
@@ -6,5 +10,23 @@ void stl_func() {
   2) algorithms
   3) iterators
   */
+
+  std::vector<int> vec1{1, 2, 3, 4};
+  // (amount of variable, default value)
+  std::vector<int> vec2(10, 10000);
+
+  fmt::print(fg(fmt::color::green), "vec1: ");
+  for (auto &&el : vec1) {
+    fmt::print(fg(fmt::color::green), "{} ", el);
+  }
+
+  fmt::print(fg(fmt::color::green), "\nvec2: ");
+  print_raw_array(vec2.data(), vec2.size());
+}
+
+template <typename T> void print_raw_array(const T *p, size_t size) {
+  for (size_t i{}; i < size; i++) {
+    fmt::print(fg(fmt::color::green), "{} ", p[i]);
+  }
 }
 } // namespace StlExercise
